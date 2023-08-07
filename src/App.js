5371e1d07ababd317bc322db6a00dbc9152da505
Mohamed <ma811forever@gmail.com>
2023-08-07 11:58:32 +0200

    Set up Deck.gl & Mapbox National parks.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import DATA from './data.json';
+import  { Map } from 'react-map-gl';
+import  DeckGL,  { GeoJsonLayer } from 'deck.gl';
+
+const MAPBOX_ACCESS_TOKEN ='pk.eyJ1IjoibW9oYW1lZGFsZGFob3VsIiwiYSI6ImNsbDBvNmIyZjAwMHkzZXA1OWxyenNqbmQifQ.qJzQG68GkZVBwvcZkjyLrw';
+const MAP_STYLE = 'https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json';
+
+const INITIAL_VIEW_STATE = {
+  latitude: 39.8283,
+  longitude: -98.5795,
+  zoom: 3,
+  bearing: 0,
+  pitch: 30
+};
 
 function App() {
+
+  const onClick = info => {
+    if (info.object) {
+      alert(`${info.object.properties.Name}`)
+    }
+  }
+
+  const layers = [
+    new GeoJsonLayer({
+      id: 'nationalParks',
+      data: DATA,
+      filled: true,
+      pointRadiusMinPixels: 5,
+      pointRadiusMaxPixels: 2000,
+      getPointRadius: f => 5,
+      getFillColor: data => data.properties.Name.includes('National Park') ? [0, 0, 0, 250] : [86, 144, 58, 250],
+      pickable: true,
+      autoHighlight: true,
+      onClick,
+    })
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DeckGL
+      initialViewState={INITIAL_VIEW_STATE}
+      controller={true}
+      layers={layers}
+      >
+      <Map mapStyle={MAP_STYLE} mapboxAccessToken={MAPBOX_ACCESS_TOKEN} />
+      </DeckGL>
   );
 }
 
